60299bd06aa364b3eb28ca19a4ab59a55a89aeb5
ManhTV7 <MAnhTV7@vingroup.net>
2020-02-07 23:50:49 +0700

    add repo

---
+++ src/main/java/com/training/store/repository/ProductCategoryRepository.java
@@ -0,0 +1,15 @@
+package com.training.store.repository;
+
+import com.training.store.domain.ProductCategory;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Spring Data  repository for the ProductCategory entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+
+}
